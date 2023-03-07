9af70c949dc34d45c54bcdafa2537d986722c9b9
ReyAdrian520 <reyesa6845@gmail.com>
2023-03-07 09:55:47 -0600

    added a new func

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import "fmt"
 
 
 
+func addNumber(x int, y int)int {
+	return x +y
+	}
+
 func main(){
 	fmt.Println("Hello, World!")
 
 
 	fmt.Println("This is my HB Assement assignment 4")
+	
+	
+	result:= addNumber(5, 10)
+	fmt.Printf("I created a func that adds 2 numbers together: %v", result)
 }
+
